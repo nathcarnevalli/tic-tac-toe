585bdbc8edc5018f6866e6749383f0813c56c0a8
nathcarnevalli <nathaliacarnevalli004@gmail.com>
2023-06-28 17:35:10 -0300

    feat: add player's name and start the game

--- src/js/game.js
+++ src/js/game.js
@@ -7,4 +7,35 @@ const createGameBoard = () => {
   }
 }
 
-createGameBoard()
+const Player = (name, figure, count, isPlaying) => {
+  return { name, figure, count, isPlaying }
+}
+
+const round = []
+
+const playerOne = Player('', 'x', 0, true)
+
+const playerTwo = Player('', 'o', 0, false)
+
+const hide = element => {
+  element.classList.add('hidden')
+}
+
+const show = element => {
+  element.classList.remove('hidden')
+}
+
+const start = event => {
+  hide(event.target.parentElement)
+  show(event.target.parentElement.nextElementSibling)
+}
+
+const play = event => {
+  event.preventDefault()
+  playerOne.name = p1.value
+  playerTwo.name = p2.value
+  p1Name.innerText = playerOne.name
+  p2Name.innerText = playerTwo.name
+  hide(event.target.parentElement)
+  show(event.target.parentElement.nextElementSibling)
+}
