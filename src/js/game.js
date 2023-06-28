362e9ae0ab3793780a966b16ee61f38fe5d5d429
nathcarnevalli <nathaliacarnevalli004@gmail.com>
2023-06-28 18:55:14 -0300

    feat: add circle and cross to gameBoard

--- src/js/game.js
+++ src/js/game.js
@@ -2,7 +2,7 @@ const createGameBoard = () => {
   for (let i = 0; i < 9; i++) {
     const square = document.createElement('div')
     square.className = 'bg-blue-400 hover:bg-blue-500'
-    square.id = i + 1
+    square.id = i
     gameBoard.appendChild(square)
   }
 }
@@ -11,7 +11,7 @@ const Player = (name, figure, count, isPlaying) => {
   return { name, figure, count, isPlaying }
 }
 
-const round = []
+let round = ['', '', '', '', '', '', '', '', '']
 
 const playerOne = Player('', 'x', 0, true)
 
@@ -39,3 +39,26 @@ const play = event => {
   hide(event.target.parentElement)
   show(event.target.parentElement.nextElementSibling)
 }
+
+gameBoard.addEventListener('click', event => {
+  if (
+    event.target.style.backgroundImage === '' &&
+    event.target.id !== 'gameBoard'
+  ) {
+    if (playerOne.isPlaying) {
+      playerOne.isPlaying = false
+      playerTwo.isPlaying = true
+      event.target.style.backgroundImage = 'url("../src/images/x.svg")'
+      event.target.style.backgroundRepeat = 'no-repeat'
+      event.target.style.backgroundSize = 'cover'
+      round[Number(event.target.id)] = playerOne.figure
+    } else {
+      playerOne.isPlaying = true
+      playerTwo.isPlaying = false
+      event.target.style.backgroundImage = 'url("../src/images/o.svg")'
+      event.target.style.backgroundRepeat = 'no-repeat'
+      event.target.style.backgroundSize = 'cover'
+      round[Number(event.target.id)] = playerTwo.figure
+    }
+  }
+})
