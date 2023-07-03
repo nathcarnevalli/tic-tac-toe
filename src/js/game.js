eab260d1875664e0547497e6a4d1656e69218d2f
nathcarnevalli <nathaliacarnevalli004@gmail.com>
2023-07-03 18:50:01 -0300

    feat: add functions hide and show that hide or show a tag, start and play that gets the players name and starts the game, checkWinner to see the game has a winner and gameBoard click event

--- src/js/game.js
+++ src/js/game.js
@@ -2,20 +2,18 @@ const createGameBoard = () => {
   for (let i = 0; i < 9; i++) {
     const square = document.createElement('div')
     square.className = 'bg-blue-400 hover:bg-blue-500'
-    square.id = i
+    square.id = i + 1
     gameBoard.appendChild(square)
   }
 }
 
-const Player = (name, figure, count, isPlaying) => {
-  return { name, figure, count, isPlaying }
+const Player = (name, count, isPlaying, round, isWinner) => {
+  return { name, count, isPlaying, round, isWinner }
 }
 
-let round = ['', '', '', '', '', '', '', '', '']
+const playerOne = Player('', 0, true, [], false)
 
-const playerOne = Player('', 'x', 0, true)
-
-const playerTwo = Player('', 'o', 0, false)
+const playerTwo = Player('', 0, false, [], false)
 
 const hide = element => {
   element.classList.add('hidden')
@@ -40,6 +38,39 @@ const play = event => {
   show(event.target.parentElement.nextElementSibling)
 }
 
+const checkWinner = rounds => {
+  const winner = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+  ]
+
+  for (let win of winner) {
+    let countWin = 0
+    for (let pos of win) {
+      for (let round of rounds) {
+        if (round === pos) {
+          countWin++
+        }
+      }
+    }
+    if (countWin >= 3) {
+      return true
+    }
+  }
+
+  return false
+}
+
+const endGame = winner => {
+  console.log(winner)
+}
+
 gameBoard.addEventListener('click', event => {
   if (
     event.target.style.backgroundImage === '' &&
@@ -51,14 +82,29 @@ gameBoard.addEventListener('click', event => {
       event.target.style.backgroundImage = 'url("../src/images/x.svg")'
       event.target.style.backgroundRepeat = 'no-repeat'
       event.target.style.backgroundSize = 'cover'
-      round[Number(event.target.id)] = playerOne.figure
+      playerOne.round.push(Number(event.target.id))
+
+      if (checkWinner(playerOne.round)) {
+        endGame(playerOne.name)
+        playerOne.count += 1
+      }
     } else {
       playerOne.isPlaying = true
       playerTwo.isPlaying = false
       event.target.style.backgroundImage = 'url("../src/images/o.svg")'
       event.target.style.backgroundRepeat = 'no-repeat'
       event.target.style.backgroundSize = 'cover'
-      round[Number(event.target.id)] = playerTwo.figure
+      playerTwo.round.push(Number(event.target.id))
+      playerTwo.isWinner = checkWinner(playerTwo.round)
+
+      if (checkWinner(playerOne.round)) {
+        endGame(playerTwo.name)
+        playerTwo.count += 1
+      }
     }
+  }
+
+  if (playerOne.round.length > 5 || playerTwo.round.length > 4) {
+    endGame('Tie')
   }
 })
